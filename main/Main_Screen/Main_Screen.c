c1c39ff874c047e32e27798c249bfa89ce314752
Orense001 <edwinstalin@hotmail.es>
2023-06-01 14:14:14 -0500

    Se elimina pines para que no haya conflictos con módulos

--- main/Main_Screen/Main_Screen.c
+++ main/Main_Screen/Main_Screen.c
@@ -75,7 +75,10 @@ void Main_Screen( void * pvParameters )
     //xSemaphoreTake(LlaveGlobal, portMAX_DELAY);
     ///                     R1  R2  R3  R4  C1  C2  C3  C4 
     //gpio_num_t keypad[8] = {13, 12, 14, 27, 26, 25, 33, 32};
-    gpio_num_t keypad[8] = {16, 17, 5, 27, 26, 25, 33, 32};
+    //gpio_num_t keypad[8] = {16, 17, 5, 27, 26, 25, 33, 32};
+    gpio_num_t keypad[8] = {27, 26, 25, 33, 32, 2, 16, 17};
+    //gpio_num_t keypad[8] = {39, 34, 35, 32, 33, 25, 26, 27};
+
     keypad_initalize(keypad); /// Inicializa keyboard
     RTC_init(&s_dev); // Inizializa el i2c
     //Time_config(&time_tc); //Aqui se configura la hora. El usuario hace esto. TODO: hay que reemplazar por teclado.
