ecc77d36c10cec63b9b1eeeffacc665952ee6bd1
Orense001 <edwinstalin@hotmail.es>
2023-08-04 09:51:26 -0500

    PWM modificado para motor

--- main/Pwm_motor/Pwm_motor.h
+++ main/Pwm_motor/Pwm_motor.h
@@ -23,14 +23,25 @@ extern "C" {
 
 
 /******************************** (3) DEFINES & MACROS *******************************************/
-#define LEDC_TIMER              LEDC_TIMER_0
+/*#define LEDC_TIMER              LEDC_TIMER_0
 #define LEDC_MODE               LEDC_LOW_SPEED_MODE
-#define LEDC_OUTPUT_IO          (5) // Define the output GPIO
+#define LEDC_OUTPUT_IO          (2) // Define the output GPIO
 #define LEDC_CHANNEL            LEDC_CHANNEL_0
 #define LEDC_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
 #define LEDC_DUTY_50               (4095) // Set duty to 50%. ((2 ** 13) - 1) * 50% = 4095
 #define LEDC_DUTY_0               (0) // Set duty to 0%.
-#define LEDC_FREQUENCY          (5000) // Frequency in Hertz. Set frequency at 5 kHz
+#define LEDC_FREQUENCY          (5000) // Frequency in Hertz. Set frequency at 5 kHz*/
+
+#define LEDC_TIMER            LEDC_TIMER_0
+#define LEDC_MODE             LEDC_HIGH_SPEED_MODE
+#define LEDC_OUTPUT_IO        (2) // Define the output GPIO
+#define LEDC_CHANNEL          LEDC_CHANNEL_0
+#define LEDC_DUTY_RES         LEDC_TIMER_15_BIT // Set duty resolution to 15 bits for smoother motion
+#define LEDC_DUTY_MIN         (2000) // Set duty for minimum position (1 ms pulse)
+#define LEDC_DUTY_MAX         (4000) // Set duty for maximum position (2 ms pulse)
+#define LEDC_DUTY_50  ((LEDC_DUTY_MAX + LEDC_DUTY_MIN) / 2) // Set duty for 50% position (1.5 ms pulse)
+#define LEDC_DUTY_0               (0) // Set duty to 0%.
+#define LEDC_FREQUENCY        (50) // Frequency in Hertz. Set frequency at 50 Hz for servomotors
 
 /*********************************** (4) PRIVATE VARS ********************************************/
 
